cdae8d18e2df87d2e006d94f7ddd2485a6391452
Diego Dell'Era <ddellera@socialmetrix.com>
2016-09-30 00:33:09 -0300

    week3

---
+++ week3/ex_product_rule_bayes.py
@@ -0,0 +1,13 @@
+healthy = 0.999
+infected = 0.001
+positive_if_healthy = 0.01
+negative_if_healthy = 0.99
+positive_if_infected = 0.99
+negative_if_infected = 0.01
+
+healthy * positive_if_healthy
+healthy * negative_if_healthy
+infected * positive_if_infected
+infected * negative_if_infected
+infected * negative_if_infected
+
